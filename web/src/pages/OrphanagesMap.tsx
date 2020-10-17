6515ba0214ac7f8d09e9f918d4947a869bd6f20e
Luiz Felipe <luizfelipeduartealves@outlook.com>
2020-10-16 23:09:53 -0400

    feat: Added points on map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import Leaflet from 'leaflet';
 
-import {Map, TileLayer} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-import {FiPlus} from 'react-icons/fi';
+import {FiPlus, FiArrowRight} from 'react-icons/fi';
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
 
 function OrphanagesMap() {
     return (
@@ -35,6 +44,18 @@ function OrphanagesMap() {
                 <TileLayer 
                     url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
+
+                <Marker
+                    icon={mapIcon}
+                    position={[-3.0777533,-60.0189982]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        SHOW!!
+                        <Link to="">
+                            <FiArrowRight size={25} color="#FFF" />
+                        </Link>
+                    </Popup>
+                </Marker>
             </Map>
 
             <Link to="" className="create-orphanage">
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import Orphanages from './pages/OrphanagesMap';
+import OrphanegeCreate from './pages/CreateOrphanage';
+import OrphanageProfile from './pages/Orphanage';
 
 function Routes(){
     return(
@@ -10,6 +12,8 @@ function Routes(){
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={Orphanages} />
+                <Route path="/orphanage/create" component={OrphanegeCreate} />
+                <Route path="/orphanage/:id" component={OrphanageProfile} />
             </Switch>
         </BrowserRouter>
     )
